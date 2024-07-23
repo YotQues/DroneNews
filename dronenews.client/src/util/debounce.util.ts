38a9958467625bfc6cc12c0f7c9dd089de812772
YotamAran <yotamaran@applydesign.io>
2024-07-23 18:53:23 +0300

    debounce util

---
+++ dronenews.client/src/util/debounce.util.ts
@@ -0,0 +1,11 @@
+export function debounce<TFunc extends (...args: unknown[]) => void>(callback: TFunc, wait = 0) {
+    let timeout: number | undefined = undefined;
+    return ((...args: unknown[]) => {
+        timeout && window.clearTimeout(timeout);
+        timeout = window.setTimeout(() => {
+            callback(...args);
+            timeout && window.clearTimeout(timeout);
+            timeout = undefined;
+        }, wait);
+    }) as TFunc
+}

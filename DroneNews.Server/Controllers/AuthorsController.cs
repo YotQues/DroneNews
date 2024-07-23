541c2b54a66929bde2b9c4426910c153c521094e
YotamAran <yotamaran@applydesign.io>
2024-07-23 18:11:00 +0300

    authors + sources controllers

---
+++ DroneNews.Server/Controllers/AuthorsController.cs
@@ -0,0 +1,44 @@
+﻿using DroneNews.Contract.Dto;
+using DroneNews.QueryHandlers.Authors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DroneNews.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthorsController(AuthrosQueryHandler queryHandler, ILogger<AuthorsController> logger) : ControllerBase
+{
+private readonly AuthrosQueryHandler queryHandler = queryHandler;
+    private readonly ILogger<AuthorsController> _logger = logger;
+
+[HttpGet(Name = "FetchAuthors")]
+[ProducesResponseType(typeof(ListResponse<AuthorDto>), 200)]
+public async Task<IActionResult> GetAuthors([FromQuery] GetAuthorsQuery query)
+{
+    try
+    {
+        Log("Entered");
+        var (page, pageSize, search) = query;
+
+        Log("Sending Query");
+        var res = await queryHandler.Handle(new((page - 1) * pageSize, pageSize, search));
+
+        Log("Success");
+        return Ok(res);
+
+    }
+    catch (Exception ex)
+    {
+        Log($"Failed: {ex.Message}");
+        return StatusCode(500, new { ex.Message });
+    }
+}
+private void Log(string message)
+{
+
+    _logger.LogInformation($"[{HttpContext.Request.Path}] / {HttpContext.Request.Method} - {message}");
+}
+
+}
+public record GetAuthorsQuery(int page = 1, int pageSize = 10, string? search = null);
---
+++ DroneNews.Server/Controllers/SourcesController.cs
@@ -0,0 +1,44 @@
+﻿using DroneNews.Contract.Dto;
+using DroneNews.QueryHandlers.Sources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DroneNews.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SourcesController(SourcesQueryHandler queryHandler, ILogger<SourcesController> logger) : ControllerBase
+    {
+    private readonly SourcesQueryHandler queryHandler = queryHandler;
+        private readonly ILogger<SourcesController> _logger = logger;
+
+    [HttpGet(Name = "FetchSources")]
+    [ProducesResponseType(typeof(ListResponse<SourceDto>), 200)]
+    public async Task<IActionResult> GetSources([FromQuery] GetSourcesQuery query)
+    {
+        try
+        {
+            Log("Entered");
+            var (page, pageSize, search) = query;
+
+            Log("Sending Query");
+            var res = await queryHandler.Handle(new((page - 1) * pageSize, pageSize, search));
+
+            Log("Success");
+            return Ok(res);
+
+        }
+        catch (Exception ex)
+        {
+            Log($"Failed: {ex.Message}");
+            return StatusCode(500, new { ex.Message });
+        }
+    }
+    private void Log(string message)
+    {
+
+        _logger.LogInformation($"[{HttpContext.Request.Path}] / {HttpContext.Request.Method} - {message}");
+    }
+
+    }
+}
+public record GetSourcesQuery(int page = 1, int pageSize = 10, string? search = null);

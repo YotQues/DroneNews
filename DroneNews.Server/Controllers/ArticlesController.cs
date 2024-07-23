7a32d84f87d4b4967b11e52a8289947821dcbc8d
YotamAran <yotamaran@applydesign.io>
2024-07-23 16:57:32 +0300

    Articles Controller

---
+++ DroneNews.Server/Controllers/ArticlesController.cs
@@ -0,0 +1,45 @@
+﻿using DroneNews.Contract.Dto;
+using DroneNews.QueryHandlers.Articles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DroneNews.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ArticlesController(ArticlesQueryHandler queryHandler, ILogger<ArticlesController> logger) : ControllerBase
+{
+    private readonly ArticlesQueryHandler queryHandler = queryHandler;
+    private readonly ILogger<ArticlesController> _logger = logger;
+
+    [HttpGet(Name = "FetchArticles")]
+    [ProducesResponseType(typeof(ListResponse<AuthorDto>), 200)]
+    public async Task<IActionResult> GetArticles([FromQuery] GetArticlesQuery query)
+    {
+        void log(string message) => Log(HttpContext.Request.Path, HttpContext.Request.Method, message);
+        try
+        {
+            log("Entered");
+            var (search, source, author, page) = query;
+
+            log("Sending Query");
+            var res = await queryHandler.Handle(new(page - 1, 20, source, author, search));
+
+            log("Success");
+            return Ok(res);
+
+        }
+        catch (Exception ex)
+        {
+            log($"Failed: {ex.Message}");
+            return StatusCode(500, new { ex.Message });
+        }
+    }
+    private void Log(string route, string method, string message)
+    {
+
+        _logger.LogInformation($"[{method}] / {route} - {message}");
+    }
+
+}
+
+public record GetArticlesQuery(string? search = null, int? source = null, int? author = null, int page = 1);

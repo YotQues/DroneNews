c8ae448d60d755f61f84d851c935f5bea09f8ca1
YotamAran <yotamaran@applydesign.io>
2024-07-23 23:11:01 +0300

    useArticles works

--- dronenews.client/src/server/hooks/useArticles.ts
+++ dronenews.client/src/server/hooks/useArticles.ts
@@ -1,33 +1,61 @@
-import {useInfiniteQuery} from "@tanstack/react-query";
-import {useState} from "react";
-import {ArticleDto, ListResponse} from "../model";
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { debounce } from '../../utils/debounce.util';
+import { getSearchParams } from '../../utils/getSearchParams.ts';
+import type { Article, ListResponse } from '../model';
 
 export function useArticles() {
-    const [search, setSearch] = useState("");
-    const {data, isFetching, fetchNextPage} = useInfiniteQuery({
-        getNextPageParam<TQueryFnData, TPageParam>(lastPage: TQueryFnData, allPages: Array<TQueryFnData>, lastPageParam: TPageParam, allPageParams: Array<TPageParam>): TPageParam | undefined | null {
-            return lastPageParam + 1;
-        },
-        initialData: undefined,
-        queryKey: ['articles', {search}],
-        queryFn: ({pageParam}) => populateArticles({search: search, page: pageParam as number}),
-        initialPageParam: 1,
-    });
+  const [search, setSearch] = useState('');
+  const [source, setSource] = useState<number | null>(null);
+  const [author, setAuthor] = useState<number | null>(null);
+  const [isEndOfData, setIsEndOfData] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+  const [totalItems, setTotalItems] = useState(0);
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState<Article[]>([]);
+  const [fetchTrigger, triggerFetch] = useState(Symbol());
 
-    function populateArticles({search, page = 1, author, source}: {
-        search: string,
-        page?: number,
-        author?: number,
-        source?: number
-    }) {
-        return fetch(`api/articles?page=${page}&search=${search}&author=${author}&source=${source}`).then(res => res.json() as Promise<ListResponse<ArticleDto>>);
-    }
+  const fetchArticles = useCallback(() => {
+    setIsFetching(true);
+    return fetch(
+      `api/articles?${getSearchParams({
+        search,
+        page,
+        author,
+        source
+      }).toString()}`
+    )
+      .then((res) => res.json() as Promise<ListResponse<Article>>)
+      .then((resp) => {
+        setIsEndOfData(resp.isLastPage);
+        setTotalItems(resp.totalItems);
+        setData((prev) => (page == 1 ? resp.items : [...prev, ...resp.items]));
+        setIsFetching(false);
+      });
+  }, [search, page, author, source]);
 
-    return {
-        articles: data,
-        setSearch,
-        isFetching,
-        fetchNextPage,
+  useEffect(() => {
+    setPage(1);
+    triggerFetch(Symbol());
+  }, [search, source, author]);
 
-    }
+  useEffect(() => {
+    const timeout = window.setTimeout(() => {
+      fetchArticles();
+    }, 300);
+    return () => {
+      timeout && window.clearTimeout(timeout);
+    };
+  }, [fetchTrigger, fetchArticles]);
+
+  return useMemo(() => ({
+    articles: data,
+    setSearch,
+    setAuthor,
+    setSource,
+    isFetching,
+    fetchNextPage: () => !isEndOfData && setPage((curr) => curr + 1),
+    isEndOfData,
+    totalItems,
+    source
+  }), [data, isEndOfData, isFetching, source, totalItems]);
 }

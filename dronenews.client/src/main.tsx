7ac0ef6028c9407e6a22d0d44b6a266ce472d184
YotamAran <yotamaran@applydesign.io>
2024-07-24 01:41:58 +0300

    removed remains of react-query

--- dronenews.client/src/main.tsx
+++ dronenews.client/src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {App} from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <QueryClientProvider client={new QueryClient()}>
             <App/>
-        </QueryClientProvider>
     </React.StrictMode>
 );

37f59dce1e352ed416ce006970364d86785d439e
YotamAran <yotamaran@applydesign.io>
2024-07-23 12:50:24 +0300

    Model / Migrations / OriginalUrl as uniqueIndex

--- Model/Migrations/20240723094505_Articles_OriginalUrl_Title_UniqueIndex.Designer.cs
+++ Model/Migrations/20240723094505_Articles_OriginalUrl_Title_UniqueIndex.Designer.cs
@@ -4,6 +4,7 @@ using DroneNews.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DroneNews.Model.Migrations
 {
     [DbContext(typeof(DroneNewsContext))]
-    partial class DroneNewsContextModelSnapshot : ModelSnapshot
+    [Migration("20240723094505_Articles_OriginalUrl_Title_UniqueIndex")]
+    partial class Articles_OriginalUrl_Title_UniqueIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Model/Migrations/20240723094505_Articles_OriginalUrl_Title_UniqueIndex.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DroneNews.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class Articles_OriginalUrl_Title_UniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_Title",
+                table: "Articles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalUrl",
+                table: "Articles",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_OriginalUrl",
+                table: "Articles",
+                column: "OriginalUrl",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_Title",
+                table: "Articles",
+                column: "Title",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_OriginalUrl",
+                table: "Articles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_Title",
+                table: "Articles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalUrl",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_Title",
+                table: "Articles",
+                column: "Title");
+        }
+    }
+}
